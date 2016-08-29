d1b5dc5aa1bae0815cda9b1e2f42937c4244b89c
yui0722 <takahaimasato222@gmail.com>
2016-08-29 09:55:31 +0900

    追加commit

---
+++ src/app.js
@@ -0,0 +1,149 @@
+var size;
+var gameScene = cc.Scene.extend({
+  onEnter: function() {
+    this._super();
+    var layer0 = new fieldLayer();
+    var layer1 = new gameLayer();
+    var layer2 = new charaLayer();
+    var layer3 = new particleLayer();
+    this.addChild(layer0);
+    this.addChild(layer1);
+    this.addChild(layer2);
+    this.addChild(layer3);
+
+  }
+});
+
+//デバッグ用ラベル
+var debugText;
+var gameLayer = cc.Layer.extend({
+  sprite: null,
+  ctor: function() {
+    this._super();
+    size = cc.winSize;
+    //デバッグ用ラベルをcreate
+    debugText = cc.LabelTTF.create("debug","Arial","32", cc.TEXT_ALIGNMENT_CENTER);
+    this.addChild(debugText);
+    debugText.setPosition(450,size.height - 20);
+    return true;
+  },
+
+});
+
+var fieldLayer = cc.Layer.extend({
+  ctor: function() {
+    this._super();
+
+    var size = cc.director.getWinSize();
+
+    var sprite = cc.Sprite.create(res.ss_BattleScene_bg1);
+    sprite.setPosition(size.width / 2, size.height / 2);
+    sprite.setScale(0.8);
+    this.addChild(sprite, 0);
+  }
+});
+
+var charaLayer = cc.Layer.extend({
+  ctor: function() {
+    this._super();
+
+    var size = cc.director.getWinSize();
+
+    //水キャラクターを追加
+    var sprite11 = cc.Sprite.create(res.chara_princessselect_11);
+    sprite11.setPosition(size.width * 0.1, size.height * 0.4);
+    sprite11.setScale(0.8);
+    this.addChild(sprite11, 0);
+
+    //火属性　敵ｻｺ中ボスキャラクター
+    var sprite4 = cc.Sprite.create(res.chara_enemy_4);
+    sprite4.setPosition(size.width * 0.4, size.height * 0.40);
+    sprite4.setScale(1.2);
+    this.addChild(sprite4, 0);
+  }
+});
+
+//パーティクル用のレイヤー
+var particleLayer = cc.Layer.extend({
+  skillSelect: 0,
+  skillLevel: 1,
+  skillCnt: 1,
+
+  ctor: function() {
+    this._super();
+    size = cc.winSize;
+    this.scheduleUpdate();
+    return true;
+  },
+  update: function(_dt) {
+
+
+    if (this.skillCnt == 1) {
+
+      debugText.setString("this.skillCnt:"+this.skillCnt
+      + " skillSelect:"+this.skillSelect
+      + " skillLevel:"+this.skillLevel
+    );
+
+     this.skillParticle(this.skillSelect, this.skillLevel, 350, 100);
+     //debug
+     //this.skillParticle(2,4, 350, 100);
+
+    }
+    if ((this.skillCnt % 80) == 0) {
+      this.skillCnt = 0;
+      this.skillLevel++
+      //HealとSlipスキル追加
+      if(this.skillSelect<3) {
+        this.skillLevel = this.skillLevel  % 1;
+      } else {
+        this.skillLevel = this.skillLevel  % 2;
+      }
+
+      this.removeAllChildren();
+      if (this.skillLevel == 0) {
+        this.skillLevel++;
+        this.skillSelect++;
+        this.skillSelect = this.skillSelect % 4;
+      }
+
+    }
+    //フレームをカウントする
+    this.skillCnt++;
+    /*
+    debugText.setString("this.skillCnt:"+this.skillCnt
+    + " skillSelect:"+this.skillSelect
+    + " skillLevel:"+this.skillLevel);
+*/
+  },
+
+//属性とスキルレベルと座標を与えてパーティクルを生成する関数
+  skillParticle: function(attrib, rare, x, y) {
+
+    //debugText.setString("attrib:"+attrib);
+  　　//HealとSlipスキル追加
+    var skillName = ["hibasira", "water", "word","yami"];
+    var sName = "res." + skillName[attrib] + "Texture" + rare + "_plist";
+    var sName2 = "res." + skillName[attrib] + "Texture2" + rare + "_plist";
+    var sName3 = "res." + skillName[attrib] + "Texture3" + rare + "_plist";
+
+
+    debugText.setString(sName);
+
+    var tempParticle = new cc.ParticleSystem(eval(sName));
+    tempParticle.setPosition(x, y);
+    this.addChild(tempParticle, 20);
+    tempParticle.setAutoRemoveOnFinish(true);
+
+
+    var tempParticle = new cc.ParticleSystem(eval(sName2));
+    tempParticle.setPosition(x, y);
+    this.addChild(tempParticle, 20);
+    tempParticle.setAutoRemoveOnFinish(true);
+
+    var tempParticle = new cc.ParticleSystem(eval(sName3));
+    tempParticle.setPosition(x, y);
+    this.addChild(tempParticle, 20);
+    tempParticle.setAutoRemoveOnFinish(true);
+  },
+});
---
+++ src/resource.js
@@ -0,0 +1,47 @@
+var res = {
+  HealTexture_png: "res/Heal_texture.png",
+  DotTexture_png: "res/Dot_texture.png",
+  particle_texture: "res/Dot_texture.png",
+  chara_princessselect_10: "res/char/chara_princessselect_10.png",
+  chara_princessselect_11: "res/char/chara_princessselect_11.png",
+  chara_princessselect_12: "res/char/chara_princessselect_12.png",
+  ss_BattleScene_bg1: "res/field/ss_BattleScene_bg1.jpg",
+  ss_BattleScene_bg2: "res/field/ss_BattleScene_bg2.jpg",
+  ss_BattleScene_bg3: "res/field/ss_BattleScene_bg3.jpg",
+  chara_enemy_1: "res/char/chara_enemy_1.png",
+  chara_enemy_2: "res/char/chara_enemy_2.png",
+  chara_enemy_3: "res/char/chara_enemy_3.png",
+  chara_enemy_4: "res/char/chara_enemy_4.png",
+  chara_enemy_5: "res/char/chara_enemy_5.png",
+  chara_enemy_6: "res/char/chara_enemy_6.png",
+  FireTexture0_plist: "res/effect/Fire_texture0.plist",
+  FireTexture1_plist: "res/effect/Fire_texture1.plist",
+  FireTexture2_plist: "res/effect/Fire_texture2.plist",
+  FireTexture3_plist: "res/effect/Fire_texture3.plist",
+  FireTexture4_plist: "res/effect/Fire_texture4.plist",
+  WaterTexture1_plist: "res/effect/Water_texture1.plist",
+  WaterTexture2_plist: "res/effect/Water_texture2.plist",
+  WaterTexture3_plist: "res/effect/Water_texture3.plist",
+  WaterTexture4_plist: "res/effect/Water_texture4.plist",
+  WoodTexture1_plist: "res/effect/Wood_texture1.plist",
+  WoodTexture2_plist: "res/effect/Wood_texture2.plist",
+  WoodTexture3_plist: "res/effect/Wood_texture3.plist",
+  WoodTexture4_plist: "res/effect/Wood_texture4.plist",
+  HealTexture1_plist: "res/effect/Heal_texture1.plist",
+  SlipTexture1_plist: "res/effect/Slip_texture1.plist",
+  hibasiraTexture1_plist: "res/effect/hibasira1_texture.plist",
+  hibasiraTexture21_plist: "res/effect/hibasira3_texture.plist",
+  waterTexture1_plist: "res/effect/water1_texture.plist",
+  waterTexture21_plist: "res/effect/water2_texture.plist",
+  wordTexture1_plist: "res/effect/word1_texture.plist",
+  wordTexture21_plist: "res/effect/word2_texture.plist",
+  yamiTexture1_plist: "res/effect/yami1_texture.plist",
+  yamiTexture21_plist: "res/effect/yami3_texture.plist",
+  yamiTexture31_plist: "res/effect/yami4_texture.plist",
+
+};
+
+var g_resources = [];
+for (var i in res) {
+  g_resources.push(res[i]);
+}
